d1ed77e92e753850653d226296fecd8f1519df4c
Alison Cordeiro <acordeiro@gmail.com>
2022-05-09 12:50:12 -0700

    Add text input for SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 // Only use render when extending a React Component.
 // When declaring a component, just straight to return.
 const App = () => {
-    return <p>Hello</p>;
+    return <div><SearchBar /></div>;
 }
 
 export default App;
