0095633fefba0a6d48e339db3e219c841c34630b
Alison Cordeiro <acordeiro@gmail.com>
2022-05-09 12:43:43 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Only use render when extending a React Component.
+// When declaring a component, just straight to return.
+const App = () => {
+    return <p>Hello</p>;
+}
+
+export default App;
