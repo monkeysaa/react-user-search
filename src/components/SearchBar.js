5d6fa726cc05378f729a8f47052be66e85ff1b27
Alison Cordeiro <acordeiro@gmail.com>
2022-05-09 13:21:29 -0700

    Refactor to a controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value)
-  }
+  // onInputChange(event) {
+  //   console.log(event.target.value)
+  // }
+  state = { term: '' };
 
   render() {
     return (
@@ -11,13 +12,31 @@ class SearchBar extends React.Component {
         <form className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            {/* Note: no parens if onInputChange to be called asynch */}
-            <input type="text" onChange={this.onInputChange}/>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
     );
   }
 }
+
+
+//UNCONTROLLED ELEMENT
+// This means the data is stored in the HTML elements, vs. in React Components
+//     <input type="text" onChange={(e)=> console.log(e.target.value)} />
+
+// CONTROLLED ELEMENT
+  // state = { term: '' };
+  // *LATER*
+  {/* <input
+type="text"
+value={this.state.term}
+onChange={e=> this.setState({ term: e.target.value })}
+/> */}
+// Every keystroke, the render method called and the value re-renders.
 
 export default SearchBar;
