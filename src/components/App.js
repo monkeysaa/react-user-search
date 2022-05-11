4a66e1abce0c672bd50f84a3cfd22eb1abe8c5b3
Alison Cordeiro <acordeiro@gmail.com>
2022-05-10 21:59:33 -0700

    Add Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 // Only use render when extending a React Component.
 // When declaring a component, just straight to return.
-const App = () => {
-  return (
-    <div className='ui container' style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
+
+// root url for unsplash: https://api.unsplash.com/
+// Found under Schema: Location under Unsplash
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID By9u1-xPR1x2JTSOWkQEkboV5-mb09FJxJBSk3drlfM'
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
+// const App = () => {
+//   return (
+//     <div className='ui container' style={{ marginTop: '10px' }}>
+//       <SearchBar />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // onInputChange(event) {
-  //   console.log(event.target.value)
-  // }
+  // Takes in a key in the SearchBar
   state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Image Search</label>
             <input
@@ -32,11 +36,26 @@ class SearchBar extends React.Component {
 // CONTROLLED ELEMENT
   // state = { term: '' };
   // *LATER*
-  {/* <input
-type="text"
-value={this.state.term}
-onChange={e=> this.setState({ term: e.target.value })}
-/> */}
+
 // Every keystroke, the render method called and the value re-renders.
+
+// onFormSUBMIT
+  // DON'T DO IT LIKE THIS, because in this setup, "this" will be broken
+  // UNLESS
+  // you build an arrow function into the code where onFormSubmit is written:
+  //     <form onSubmit={(event) => this.onFormSubmit(event)} className...
+  // Confused? Review Lessons 89 & 90 again.
+
+  // onFormSubmit(event) {
+  //   event.preventDefault();
+
+  //   console.log(this.state)
+  // }
+
+  // function above is shorthand for
+  // onFormSubmit: function(event) {
+  //  event.preventDefault()
+  //  console.log(this.state)
+  // }
 
 export default SearchBar;
