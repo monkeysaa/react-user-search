3de63729554dc731dd0c5597422eaccd5a7c003a
Alison Cordeiro <acordeiro@gmail.com>
2022-05-16 22:49:03 -0700

    Add ImageList css and WIP: ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,17 @@
+import './ImageList.css';
+import ImageCard from './ImageCard';
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   });
   // DESTRUCTURED VERSION of THE CODE BELOW
   // const images = props.images.map(image => {
   //   return <img key={image.id} src={image.urls.regular} alt={image.description} />
   // });
 
-  return <div>{images}</div>;
+  return <div className={"image-list"}>{images}</div>;
 };
 
 export default ImageList;
